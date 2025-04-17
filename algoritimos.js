a3ce50b19f8c87977df3071ff5aed33427c940b2
Leonardo10115 <leobernardes351@gmail.com>
2025-04-17 19:12:16 -0300

    Create algoritimos.js

---
+++ algoritimos.js
@@ -0,0 +1,64 @@
+let peso = 80
+let altura = 1.75
+let calculo = altura*altura
+let calculo2 = peso/calculo
+if (calculo2>26){
+console.log("Você esta acima do peso")
+
+}
+else if(calculo2<20){
+    console.log("Vai tomar bomba que é mais facil")
+
+}
+else if(calculo2 === 21 || calculo2 === 22 || calculo2 === 23){
+    console.log("Está no ideal")
+}
+
+//Nota
+let nota1 = 5
+let nota2 = 11
+let nota3 = 7
+let soma = nota1+nota2+nota3
+let media = soma/3
+
+if (media>=9){
+    console.log("Nota A")
+}
+else if (media >=9){
+
+}
+
+
+
+
+//Carro
+
+let idade = 18
+let medio = true
+if (idade>=18 && medio){
+    console.log("pode ter um carro")
+}
+else{
+    console.log("Não pode ter o carro")}{
+
+    }
+
+    
+
+//for=loop
+let 1 = 0
+for(let 1 = 0; 1<100; 1++){
+    console.log(1)
+}
+
+let valor = 0
+let i = 0 
+while(i <=10){
+   valor=i*2
+}
+
+let numero = 0
+for(let i = 0; i<100)
+
+
+
